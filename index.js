00b8978b2288f8103951092ea386d79f84d23d3d
Darien <65925033+darmoment@users.noreply.github.com>
2022-05-31 19:52:35 -0400

    chaning node style

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 particlesJS("particles-js", {
     "particles": {
       "number": {
-        "value": 10,
+        "value": 380,
         "density": {
           "enable": true,
           "value_area": 800
@@ -45,7 +45,7 @@ particlesJS("particles-js", {
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#232323",
         "opacity": 0.4,
         "width": 1
       },
