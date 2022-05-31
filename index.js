e5f28d7832bbbbc533acd08331012b6e6afc79f4
Darien <65925033+darmoment@users.noreply.github.com>
2022-05-31 19:34:46 -0400

    just wanna see something

--- index.js
+++ index.js
@@ -1,103 +1,134 @@
-var canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext('2d');
+/* ---- particles.js config ---- */
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-var stars = [], // Array that contains the stars
-    FPS = 60, // Frames per second
-    x = 100, // Number of stars
-    mouse = {
-      x: 0,
-      y: 0
-    };  // mouse location
-
-// Push stars to array
-
-for (var i = 0; i < x; i++) {
-  stars.push({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    radius: Math.random() * 1 + 1,
-    vx: Math.floor(Math.random() * 50) - 25,
-    vy: Math.floor(Math.random() * 50) - 25
-  });
-}
-
-// Draw the scene
-
-function draw() {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-  
-  ctx.globalCompositeOperation = "lighter";
-  
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var s = stars[i];
-  
-    ctx.fillStyle = "#fff";
-    ctx.beginPath();
-    ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.fillStyle = 'black';
-    ctx.stroke();
-  }
-  
-  ctx.beginPath();
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var starI = stars[i];
-    ctx.moveTo(starI.x,starI.y); 
-    if(distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
-    for (var j = 0, x = stars.length; j < x; j++) {
-      var starII = stars[j];
-      if(distance(starI, starII) < 150) {
-        //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
-        ctx.lineTo(starII.x,starII.y); 
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 380,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
       }
-    }
-  }
-  ctx.lineWidth = 0.05;
-  ctx.strokeStyle = 'white';
-  ctx.stroke();
-}
-
-function distance( point1, point2 ){
-  var xs = 0;
-  var ys = 0;
- 
-  xs = point2.x - point1.x;
-  xs = xs * xs;
- 
-  ys = point2.y - point1.y;
-  ys = ys * ys;
- 
-  return Math.sqrt( xs + ys );
-}
-
-// Update star locations
-
-function update() {
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var s = stars[i];
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 140,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
   
-    s.x += s.vx / FPS;
-    s.y += s.vy / FPS;
-    
-    if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
-    if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
-  }
-}
-
-canvas.addEventListener('mousemove', function(e){
-  mouse.x = e.clientX;
-  mouse.y = e.clientY;
-});
-
-// Update and draw
-
-function tick() {
-  draw();
-  update();
-  requestAnimationFrame(tick);
-}
-
-tick();
+  
+  /* ---- stats.js config ---- */
+  
+  var count_particles, stats, update;
+  stats = new Stats;
+  stats.setMode(0);
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+  document.body.appendChild(stats.domElement);
+  count_particles = document.querySelector('.js-count-particles');
+  update = function() {
+    stats.begin();
+    stats.end();
+    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    }
+    requestAnimationFrame(update);
+  };
+  requestAnimationFrame(update);
+  
