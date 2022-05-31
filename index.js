9b030277c2f8144ff2540f8cb2e371117bb9ba72
Darien <65925033+darmoment@users.noreply.github.com>
2022-05-31 19:49:28 -0400

    just wanna see something

--- index.js
+++ index.js
@@ -108,14 +108,3 @@ particlesJS("particles-js", {
   
   
   /* ---- stats.js config ---- */
-  
-  var count_particles, stats, update;
-  stats = new Stats;
-  stats.setMode(0);
-  stats.domElement.style.position = 'absolute';
-  stats.domElement.style.left = '0px';
-  stats.domElement.style.top = '0px';
-  document.body.appendChild(stats.domElement);
-  
-  requestAnimationFrame(update);
-  
