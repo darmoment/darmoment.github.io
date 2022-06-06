46cb540c3da39d0d5048adab8cb7ad6d13c1b96c
Darien <65925033+darmoment@users.noreply.github.com>
2022-06-06 18:58:07 -0400

    nav design

--- index.js
+++ index.js
@@ -217,7 +217,7 @@ particlesJS("particles-js2", {
 
 
 
-var header = document.querySelector("#particles-js2");
+var header = document.querySelector("#particles-js");
 var stickyNav = document.querySelector("#nav");
 
 // TODO: throttle this function for optimal performance in production
