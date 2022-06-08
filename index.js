f3245baec271679080d19b7f716cd23475e2c8c9
Darien <65925033+darmoment@users.noreply.github.com>
2022-06-07 21:29:39 -0400

    skills design

--- index.js
+++ index.js
@@ -233,4 +233,6 @@ window.addEventListener('scroll', function(e){
   }
 });
 
+/*
 document.getElementById("wrapper").style.top = (document.getElementById("particles-js").offsetHeight + document.getElementById("skills").offsetHeight + 250) + "px";
+*/
