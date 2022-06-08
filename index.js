cbaa291c663e6cc2026aa4c687cbc5543bd9cd9d
Darien <65925033+darmoment@users.noreply.github.com>
2022-06-07 21:03:21 -0400

    skills design

--- index.js
+++ index.js
@@ -233,4 +233,4 @@ window.addEventListener('scroll', function(e){
   }
 });
 
-document.getElementById("wrapper").style.top = (document.getElementById("particles-js").scrollHeight + document.getElementById("skills").scrollHeight + 350) + "px";
+document.getElementById("wrapper").style.top = (document.getElementById("particles-js").scrollHeight + document.getElementById("skills").scrollHeight + 300) + "px";
