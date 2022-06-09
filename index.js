d72795df2c6c158b76805477c1816fba36371e62
Darien <65925033+darmoment@users.noreply.github.com>
2022-06-09 13:45:18 -0400

    designing contact

--- index.js
+++ index.js
@@ -232,5 +232,5 @@ function myFunction() {
 }
 
 
-document.getElementById("projectsec").style.top = (document.getElementById("particles-js").offsetHeight + document.getElementById("skills").offsetHeight + 250) + "px";
-document.getElementById("contact").style.top = (document.getElementById("particles-js").offsetHeight + document.getElementById("skills").offsetHeight + document.getElementById("projectsec").offsetHeight + 250) + "px";
+document.getElementById("projectsec").style.top = (document.getElementById("particles-js").scrollHeight + document.getElementById("skills").scrollHeight + 250) + "px";
+document.getElementById("contact").style.top = (document.getElementById("particles-js").scrollHeight + document.getElementById("skills").scrollHeight + document.getElementById("projectsec").scrollHeight + 250) + "px";
