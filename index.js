19b5e58ffbab9cffd70aa117cd8bb0c3f4de85b8
Darien <65925033+darmoment@users.noreply.github.com>
2022-06-11 16:40:11 -0400

    testing size dimensions

--- index.js
+++ index.js
@@ -238,6 +238,5 @@ function navFunction() {
 }
 
 
-/*document.getElementById("projectsec").style.top = (document.getElementById("particles-js2").scrollHeight + document.getElementById("skills").scrollHeight + 250) + "px";*/
 document.getElementById("contact").style.top = (document.getElementById("particles-js2").scrollHeight + document.getElementById("skills").scrollHeight + document.getElementById("projectsec").scrollHeight + 250) + "px";
 document.getElementById("bottom-nav").style.top = (document.getElementById("particles-js2").scrollHeight + document.getElementById("skills").scrollHeight + document.getElementById("projectsec").scrollHeight + document.getElementById("contact").scrollHeight + 250) + "px";
